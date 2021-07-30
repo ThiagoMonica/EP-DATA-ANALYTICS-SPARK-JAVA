2e172c5a43535ee1e61dae9d94ef021fe0bf25dc
ThiagoMonica <heythimx@gmail.com>
2021-07-30 00:00:45 -0300

    data plot

--- ep2.py
+++ ep2.py
@@ -2,12 +2,13 @@ from pyspark.context import SparkContext
 from pyspark.sql import column
 from pyspark.sql.session import SparkSession
 from py4j.java_gateway import JavaGateway
+import matplotlib.pyplot as plt
 
 
 # A implementar:
 # [V] demais variaveis presentes nos aquivos (por enquanto so tem 2 DEWP e TEMP)
 # [V] Filtro de intervalo de datas na tabela a ser filtrada
-# [] Grafico todo maluco ( isso fudeu pq eu n faco a menor ideia)
+# [V] Grafico todo maluco ( isso fudeu pq eu n faco a menor ideia)
 # [] Como o resultado sera agrupado ( dias, meses, anos) ?
 # [] Mais formas de calculos estatisticos - opcional (se sobrar tempo)
 
@@ -17,7 +18,7 @@ spark = SparkSession(sc)
 
 # Configuracao para permitir o input no pyspark
 scanner = sc._gateway.jvm.java.util.Scanner  
-sys_in = getattr(sc._gateway.jvm.java.lang.System, 'in')  
+sys_in = getattr(sc._gateway.jvm.java.lang.System, 'in')
 
 # Leitura do arquivo
 file_location = "./TabelaDSID.csv"
@@ -32,7 +33,6 @@ df = spark.read.format(file_type) \
   .load(file_location)
 
 def menuInformacao():
-	#print(df.schema.names[2])
 	i = -1
 	for col in df.dtypes:
 		i=i+1
@@ -40,7 +40,53 @@ def menuInformacao():
 
 # Funcao para saber o nome do tipo da informacao escolhido
 def tipoInformacao(numero):
-  return df.schema.names[int(numero)]
+	return df.schema.names[int(numero)]
+
+def calculaMedia(tabela, coluna):
+	return tabela.groupBy().sum(coluna).collect()[0][0] / tabela.count()
+
+def calculaDP(tabela, coluna):
+	media = calculaMedia(tabela, coluna)
+	variavelTipo = "tabela." + coluna
+
+	tabelaFill = tabela.select(tabela.DATE, eval(variavelTipo), ((eval(variavelTipo) - media)*(eval(variavelTipo) - media)).alias('aux'))
+	return (tabelaFill.groupBy().sum("aux").collect()[0][0]/ (tabelaFiltrada.count()-1)) ** 0.5
+
+def calculaQuadradosMin(tabela, x, y):
+	mediax = calculaMedia(tabela, x)
+	mediay = calculaMedia(tabela, y)
+
+	#variavelTipo1 = "%a.%b" % (tabela, x)
+	variavelTipo1 = "tabela." + x
+	variavelTipo2 = "tabela." + y
+
+	tabelaCalc = tabela.select(eval(variavelTipo1), (eval(variavelTipo1)*(eval(variavelTipo2)-mediay)).alias('aux = xi(yi-mediay)'), eval(variavelTipo2), (eval(variavelTipo1)*(eval(variavelTipo1)-mediax)).alias('aux = xi(xi-mediax)'))
+	b = tabelaCalc.groupBy().sum("aux = xi(yi-mediay)").collect()[0][0] / tabelaCalc.groupBy().sum("aux = xi(xi-mediax)").collect()[0][0]
+	a = mediay - b * mediax
+	
+	y0 = a + b*(tabela.groupBy().min(x).collect()[0][0])
+	y1 = a + b*(tabela.groupBy().max(x).collect()[0][0])
+	return a,b,y0,y1
+
+def plotGrafico(tabela, coluna):
+	print("Desejar vizualizar as estatisticas da primeira informacao no decorrer do tempo selecionado?")
+	print("1 \t Sim")
+	print("2 \t Nao")
+	print("Numero escolhido:")
+	escolhaGrafico = scanner(sys_in).nextLine()
+
+	if(escolhaGrafico=="1"):
+		pandasDF = tabela.toPandas()
+
+		plt.figure()
+		plt.plot(pandasDF['DATE'], pandasDF[coluna], 'g', label = "TEMP")
+		plt.axhline(y=pandasDF[coluna].mean(), color="red", label="Media")
+		plt.axhspan(pandasDF[coluna].mean() - pandasDF[coluna].std(), pandasDF[coluna].mean() + pandasDF[coluna].std(), facecolor='lavender', alpha=0.5)
+		plt.xlabel('DATE')
+		plt.ylabel(coluna)
+		plt.show()
+		return
+
 
 while (True):
 	print("\n\n******** MENU ********")
@@ -82,18 +128,17 @@ while (True):
 
 	if(metodoCalculo == "1"):
 		nomeTipo = tipoInformacao(tipoDaInfor01)
-		resultado = tabelaFiltrada.groupBy().sum(nomeTipo).collect()[0][0] / tabelaFiltrada.count()
+		resultado = calculaMedia(tabelaFiltrada, nomeTipo)
 		print("\n\nO resultado da media para o atributo", nomeTipo, "foi: \n", resultado, "\n")
+
+		plotGrafico(tabelaFiltrada, nomeTipo)
 
 	elif(metodoCalculo == "2"):
 		nomeTipo = tipoInformacao(tipoDaInfor01)
-		media = tabelaFiltrada.groupBy().sum(nomeTipo).collect()[0][0] / tabelaFiltrada.count()
-
-		# Campo do tipo que sera utilizado
-		variavelTipo = "tabelaFiltrada." + nomeTipo
-		tabelaFill = tabelaFiltrada.select(tabelaFiltrada.DATE, eval(variavelTipo), ((eval(variavelTipo) - media)*(eval(variavelTipo) - media)).alias('aux'))
-		resultado = (tabelaFill.groupBy().sum("aux").collect()[0][0]/ (tabelaFiltrada.count()-1)) ** 0.5
+		resultado = calculaDP(tabelaFiltrada, nomeTipo)
 		print("\n\nO resultado do desvio padrao para o atributo", nomeTipo, "foi: \n", resultado, "\n")
+
+		plotGrafico(tabelaFiltrada, nomeTipo)
 
 	elif(metodoCalculo == "3"):
 		print("Digite o segundo tipo de informacao")
@@ -102,21 +147,25 @@ while (True):
 		nomeTipo1 = tipoInformacao(tipoDaInfor01)
 		nomeTipo2 = tipoInformacao(tipoDaInfor02)
 
-		mediax = tabelaFiltrada.groupBy().sum(nomeTipo1).collect()[0][0] / tabelaFiltrada.count() 
-		mediay = tabelaFiltrada.groupBy().sum(nomeTipo2).collect()[0][0] / tabelaFiltrada.count() 
+		a,b,y0,y1 = calculaQuadradosMin(tabelaFiltrada, nomeTipo1, nomeTipo2)
 
-		variavelTipo1 = "tabelaFiltrada." + nomeTipo1
-		variavelTipo2 = "tabelaFiltrada." + nomeTipo2
+		print("\n\nValor b: ", b)
+		print("\n\nValor a:", a)
+		print("\n\nA reta y0 tem valor: ", y0)
+		print("\n\nA reta y1 tem valor: ", y1)
 
-		tabelaCalc = tabelaFiltrada.select(eval(variavelTipo1), (eval(variavelTipo1)*(eval(variavelTipo2)-mediay)).alias('aux = xi(yi-mediay)'), eval(variavelTipo2), (eval(variavelTipo1)*(eval(variavelTipo1)-mediax)).alias('aux = xi(xi-mediax)'))
-		b = tabelaCalc.groupBy().sum("aux = xi(yi-mediay)").collect()[0][0] / tabelaCalc.groupBy().sum("aux = xi(xi-mediax)").collect()[0][0]
-		a = mediay - b * mediax
+		pandasDF = tabelaFiltrada.toPandas()
 
+		plt.figure()
+		plt.plot(pandasDF[nomeTipo1], a + (b*pandasDF[nomeTipo1]), 'g', label = "fitted curve")
+		plt.axhline(y=y0, color="red", linestyle="--", label="Y0")
+		plt.axhline(y=y1, color="red", linestyle="--", label="Y1")
+		plt.scatter(pandasDF[nomeTipo1], pandasDF[nomeTipo2], label = "data")
+		plt.xlabel(nomeTipo1)
+		plt.ylabel(nomeTipo2)
+		plt.show()
 
-		print("\n\nO valor da variavel b calculada é: ",b)
-		print("\n\nO valor da variavel a calculada é: ",a)
-
-
+		plotGrafico(tabelaFiltrada, nomeTipo1)
 	else:
 		print("Codigo não encontrado, por favor tente novamente!")
 		continue
